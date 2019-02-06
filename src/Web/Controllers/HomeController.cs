317b415f2ddadde8179acd1b562f3e3d96f6d472
vennela-cheekoti <s533619@NWMISSOURI.EDU>
2019-02-06 10:46:36 -0600

    created new action in HomeController, Added new view(Welcome.cshtml), updated menu(added welcome in menu).

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -19,6 +19,10 @@ namespace Web.Controllers
         {
             return View();
         }
+        public IActionResult Welcome()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
